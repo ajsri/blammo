9e87d94319379ec3910242d2a066d7e22479b2d9
AJ Srivastava <aj.srivastava@monsanto.com>
2016-10-26 08:17:42 -0500

    responsive nav left

--- app/components/Overview.js
+++ app/components/Overview.js
@@ -11,6 +11,23 @@ class OverviewComponent extends Component {
 		return (
 			<div>
 				<h1>Overview</h1>
+				<p>SIMS is an inventory management system for your secured storage systems. With the app, you can keep track of:</p>
+				<ul>
+					<li>Firearms</li>
+					<li>Ammunition</li>
+					<li>Important documents</li>
+				</ul>
+				<div className="row">
+					<div className="col-md-4 col-sm-12">
+						<h2>Firearms</h2>
+					</div>
+					<div className="col-md-4 col-sm-12">
+						<h2>Ammunition</h2>
+					</div>
+					<div className="col-md-4 col-sm-12">
+						<h2>Documents</h2>
+					</div>
+				</div>
 			</div>
 		)
 	}
